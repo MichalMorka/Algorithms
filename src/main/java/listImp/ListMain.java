8661548b5782a17c0e4265a2c153d59862338774
Michal Morka <michalmorka@hotmail.com>
2019-11-30 12:39:49 +0100

    list implementations

---
+++ src/main/java/listImp/ListMain.java
@@ -0,0 +1,19 @@
+package listImp;
+
+import java.util.Random;
+
+public class ListMain {
+    public static void main(String[] args) {
+        IList<Integer> list = new ArrayList2x<>(); //zmiana ArrayList2x na LinkedList
+        Random rand = new Random();
+        int numbersCount = 10;
+        for (int i = 0; i < numbersCount; i++)
+            list.add(rand.nextInt(101)); // generujemy losowe liczby do listy
+
+        ListUtilities.display(list); // wyswietlam liste
+        list.add(11,2);
+        ListUtilities.display(list);
+        list.remove(9);
+        ListUtilities.display(list);
+    }
+}
